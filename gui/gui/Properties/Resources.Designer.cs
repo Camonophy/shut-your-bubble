f6b6d83a29d22b15615cc95e5b905cd1d697f7bb
unknown <pflugoli@b-tu.de>
2021-08-17 19:41:47 +0200

    Add pipette icon

--- gui/gui/Properties/Resources.Designer.cs
+++ gui/gui/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace gui.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Pipette {
+            get {
+                object obj = ResourceManager.GetObject("Pipette", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
